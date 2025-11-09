ec1f87e2c47274c4dc50add436621f2f9ab54942
Alexander <azunini.99@gmail.com>
2025-11-09 14:01:06 -0300

    fix: change heroImage to webp, download cv button function and change favicon

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import heroImage from "../assets/images/HeroImg.jpg";
+import HeroImage from "../assets/images/HeroImg.webp";
+import cvFile from "../assets/docs/cv.pdf";
 
 const Home: React.FC = () => {
   // hook translate
@@ -8,26 +9,36 @@ const Home: React.FC = () => {
   return (
     <div
       id="home"
-      style={{ backgroundImage: `url(${heroImage})` }}
-      className="bg-top bg-cover flex flex-col h-screen w-screen text-white md:flex-row"
+      style={{
+        backgroundImage: `url(${HeroImage})`,
+        backgroundRepeat: "no-repeat",
+      }}
+      className="bg-cover bg-top relative flex flex-col h-screen w-screen text-white md:flex-row"
     >
-      <div id="info" className="flex justify-center items-center h-screen w-screen md:w-[55%]">
-        <div className="w-[70%]">
+      <div
+        id="info"
+        className="flex justify-center items-center h-screen w-screen md:w-[55%]"
+      >
+        <div className="w-[80%]">
           <h5 className="font-[Poppins] text-rose-700 font-medium tracking-normal text-1xl mb-4">
             {t("home.welcome")}
           </h5>
           <h1 className="font-[Epilogue] font-bold mb-6 text-6xl">
             {t("home.hello")}
-            <span className="text-rose-700 block text-6xl pt-4 font-bold">
+            <span className="text-rose-700 flex text-6x1 pt-4 font-bold">
               {t("home.role")}
             </span>
           </h1>
           <p className="text-lg tracking-wide w-full text-gray-400 font-[Poppins] md:w-[65%]">
             {t("home.description")}
           </p>
-          <button className="w-42 h-12 bg-rose-700 mt-8 font-[Poppins] font-semibold shadow-sm hover:cursor-pointer hover:bg-rose-800 transition">
+          <a
+            href={cvFile}
+            download="Alexander_Zunini_CV.pdf"
+            className="inline-block w-42 h-12 bg-rose-700 mt-8 font-[Poppins] font-semibold shadow-sm hover:bg-rose-800  active:bg-rose-800 transition text-white text-center leading-12 "
+          >
             {t("home.button")}
-          </button>
+          </a>
         </div>
       </div>
     </div>
