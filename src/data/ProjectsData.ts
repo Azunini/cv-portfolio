f2d7f30d72aca3241d54677b265d91c74c39e7f0
Alexander <azunini.99@gmail.com>
2025-11-09 14:19:50 -0300

    fix: change heroImage to webp, download cv button function and change favicon

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -6,7 +6,7 @@ export const projects: Project[] = [
     id: 1,
     title: "prj1-title",
     description: "prj1-desc",
-    image: "/images/app-agenda.png",
+    image: "/images/app-agenda.webp",
     github: "https://github.com/Azunini/app-agenda",
     demo: "https://app-agenda-k2vzj1lv9-alexander-zuninis-projects.vercel.app",
   },
